3e960351734fda31e31c06f8b33a5fb338f4942a
Ryan Lewis <rlewis94@gmail.com>
2022-06-20 16:05:52 -0700

    started terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "Archive.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./pysrc/Archive.zip"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "python38"
+
+  available_memory_mb   = 1024
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "helloGET"
+  labels = {
+    my-label = "my-label-value"
+  }
+
+  environment_variables = {
+    MY_ENV_VAR = "my-env-var-value"
+  }
+}
+
+# IAM entry for a single user to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "user:myFunctionInvoker@example.com"
+}
---
+++ terraform/readme.md
@@ -0,0 +1,3 @@
+!python detect.py --source ../Road_Sign_Dataset/images/test/ --weights 
+runs/train/yolo_road_det/weights/best.pt --conf 0.25 --name 
+yolo_road_det
