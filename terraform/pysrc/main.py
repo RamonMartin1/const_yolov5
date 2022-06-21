d4741efa9db4cbeddbda3528b0837fc0b07db5fc
Martin <52055097+drax86@users.noreply.github.com>
2022-06-20 21:30:52 -0400

    working main.py

--- terraform/pysrc/main.py
+++ terraform/pysrc/main.py
@@ -1,16 +1,24 @@
 
 import torch
 import pandas as pd
+from pathlib import Path
 
+img = '/Users/ramonmartin/Documents/pytorch_projects/const_yolov5/terraform/pysrc/data/images/hd88.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+  
+
+@torch.no_grad()
 def run():
-  model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-  model.load_state_dict(torch.load('./best.pt'))
-  img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+  model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt') 
 
+  #model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+  #model.load_state_dict(torch.load('best.pt')['model'].state_dict())
+  #model.fuse()
+  #model.load_state_dict(torch.load('best.pt'))
+  #img = '/Users/ramonmartin/Documents/pytorch_projects/const_yolov5/terraform/pysrc/data/images/hd88.jpg'  # or file, Path, PIL, OpenCV, numpy, list
   # Inference
   results = model(img)
 
   # Results
-  results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+  results.save()  # or .show(), .save(), .crop(), .pandas(), etc.
 
 run()
