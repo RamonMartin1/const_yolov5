4d35d7bfd115b2da8b67bd624395eaad866458a6
Ryan Lewis <rlewis94@gmail.com>
2022-06-26 18:47:25 -0700

    tried changing json key path to relative location

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,5 +2,5 @@ provider "google" {
   project     = "yolo52"
   region      = "us-central1"
   zone        = "us-central1-c"
-  credentials = file("terraform/yolo52-922327199c2c.json")
+  credentials = file("./yolo52-922327199c2c.json")
 }
